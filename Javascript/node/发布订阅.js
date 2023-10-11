72ac328568d97504f3fcd405cc1a8bdd71f33593
hxbty <lin2770529951@163.com>
2023-10-11 10:00:44 +0800

    一个简单的发布订阅模式

--- Javascript/node/发布订阅.js
+++ Javascript/node/发布订阅.js
@@ -1,4 +1,8 @@
-let e = {
+/**
+ * 一个简单的发布订阅模式
+ * @type {{arr: *[], emit(): void, on(*): void}}
+ */
+const event = {
     arr: [],
     on(fn) {
         this.arr.push(fn)
@@ -7,13 +11,13 @@ let e = {
         this.arr.forEach(item => item())
     }
 }
-e.on(() => {
+event.on(() => {
     console.log('杰哥不要');
 })
-e.on(() => {
+event.on(() => {
     console.log('让我康康');
 })
 function fun() {
-    e.emit()
+    event.emit()
 }
 fun()
