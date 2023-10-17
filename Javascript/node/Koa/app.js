c27b5775b71c78324afc6a5409c0304a523d5f02
hxbty <lin2770529951@163.com>
2023-10-17 20:02:50 +0800

    引入静态资源处理插件

--- Javascript/node/Koa/app.js
+++ Javascript/node/Koa/app.js
@@ -1,17 +1,23 @@
 /**
- * 视图
+ * 主入口
  */
 const Koa = require("koa")
 const path = require("path")
 const bodyParser = require("koa-bodyparser")
+// 引入 koa-nunjucks-2 对渲染的模板进行转移,避免xss
 const nunjucks = require("koa-nunjucks-2")
+// 引入 koa-static 用于处理静态文件
+const staticFiles = require("koa-static")
 
 const app = new Koa()
-const router = require("./home")
+const router = require("./homeView/home")
+
+// 指定public为指定静态文件目录
+app.use(staticFiles(path.resolve(__dirname, "./public")))
 
 app.use(nunjucks({
     ext: "html",
-    path: path.join(__dirname),
+    path: path.join(__dirname, "homeView"),
     nunjucksConfig: {
         trimBlocks: true // 开启转义 防止xss
     }
