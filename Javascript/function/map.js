4f9915b5d0db5f9cd370eb285fdb01eee8f4ecdf
hxbty <lin2770529951@163.com>
2023-10-31 16:57:21 +0800

    修改错误处理

--- Javascript/function/map.js
+++ Javascript/function/map.js
@@ -1,19 +1,17 @@
 // 实现Array.map函数
 let arr = [1, 2, 3, 4, 5]
 
-Array.prototype.myMap = function (callback) {
+Array.prototype._map = function (callback) {
+  if (typeof callback !== 'function') throw new Error("callback must be a function!")
   let _length = this.length
   let _arr = []
-  if (typeof callback !== 'function') {
-    return new Error("not's a function")
-  }
   for (let index = 0; index < _length; index++) {
     _arr.push(callback.call(this, this[index], index, this))
   }
   return _arr
 }
 
-let result = arr.myMap((item, index, array) => {
+let result = arr._map((item, index, array) => {
   if (item >= 3) {
     return item
   }
