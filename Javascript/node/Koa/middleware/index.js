a8c65fe91ba8a49d8f4bfef6ecdcac34cb7a57f4
hxbty <lin2770529951@163.com>
2023-11-09 17:26:06 +0800

    配置日志中间件

--- Javascript/node/Koa/middleware/index.js
+++ Javascript/node/Koa/middleware/index.js
@@ -8,14 +8,17 @@ const nunjucks = require("koa-nunjucks-2")
 // 引入 koa-static 用于处理静态文件
 const staticFiles = require("koa-static")
 const miSend = require("./mi-send");
+const miLog = require("./mi-log")
 
 module.exports = (app) => {
-    // 指定public为指定静态文件目录
-    app.use(staticFiles(path.resolve(__dirname, "./public")))
-
+    // 注册中间件
+    app.use(miLog())
+    // 指定public为指定静态资源文件目录
+    app.use(staticFiles(path.resolve(__dirname, "../", "./public")))
     app.use(nunjucks({
         ext: "html",
-        path: path.join(__dirname, "controller", "home"),
+        // path.join(__dirname, "../") 可以获取当前项目的根目录
+        path: path.join(__dirname, "../", "public", "html"),
         nunjucksConfig: {
             trimBlocks: true // 开启转义 防止xss
         }
---
+++ Javascript/node/Koa/middleware/mi-log/logger.js
@@ -0,0 +1,57 @@
+const log4js = require("log4js");
+const access = require("./access")
+const methods = ["trace", "debug", "info", "warn", "error", "fatal", "mark"];
+const baseInfo = {
+    appLogLevel: "debug", // 指定日志级别
+    dir: "../../logs", // 指定日志存放目录
+    env: "div", // 指定当前环境
+    projectName: "Koa", // 项目名
+    serverIp: "0.0.0.0" // 默认服务器地址
+};
+const {env, appLogLevel, dir, projectName, serverIp} = baseInfo;
+// 用于存储公用日志信息
+const commonInfo = {projectName, serverIp}
+
+module.exports = () => {
+    const contextLogger = {};
+    const appenders = {}
+    appenders.cheese = {
+        type: "dateFile",
+        filename: `${dir}/cheese`, // 输出的文件名
+        pattern: "yyyy-MM-dd.log", // 文件后缀名
+        alwaysIncludePattern: true // 是否总是有后缀名
+    }
+    if (["dev", "local", "development"].includes(env)) {
+        appenders.out = {
+            type: "console"
+        }
+    }
+    let config = {
+        appenders,
+        categories: {
+            default: {
+                appenders: Object.keys(appenders),
+                level: appLogLevel
+            }
+        }
+    }
+    const logger = log4js.getLogger("cheese");
+    return async (ctx, next) => {
+        const start = Date.now();
+
+        log4js.configure(config)
+        methods.forEach((method) => {
+            contextLogger[method] = (message) => {
+                logger[method](access(ctx, message, commonInfo));
+            }
+        })
+        ctx.log = contextLogger;
+
+        await next();
+
+        const responseTime = Date.now() - start;
+        logger.info(access(ctx, {
+            responseTime: `响应时间为 -- ${responseTime / 1000}s`
+        }, commonInfo))
+    }
+}
