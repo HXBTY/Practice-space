304c6e58479e112145b8cb7e7dc44aa7a81e6368
hxbty <lin2770529951@163.com>
2023-11-21 19:46:15 +0800

    匹配自然数正则

--- regular/frequently-used.ts
+++ regular/frequently-used.ts
@@ -1,2 +1,4 @@
-// 换行符正则
-const lineBreakReg:RegExp = /\r\n|(?<!\r\n)\n(?!\r\n)|(?<!\r\n|\n)\r(?!\r\n|\n)/;
+// 匹配换行符正则
+const lineBreakReg: RegExp = /\r\n|(?<!\r\n)\n(?!\r\n)|(?<!\r\n|\n)\r(?!\r\n|\n)/;
+// 匹配自然数
+const integerReg: RegExp = /^[0-9]+$/;
